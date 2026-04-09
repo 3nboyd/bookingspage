2b5e0797c96f8576a136dd3f392ddd193b43cee6
3nboyd <noahbboyd@gmail.com>
2026-04-09 14:25:05 -0400

    Configure deployed song request endpoint

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 window.NBZ_SITE_CONFIG = {
-  songRequestEndpoint: 'https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYED_APP_ID/exec',
+  songRequestEndpoint: 'https://script.google.com/macros/s/AKfycbwkIHv_ANZjmwgCLMROoyFobpj-WTjQd5w1pMYUb5GBbut5KusPNrZtQ37OeX5FSrnN5Q/exec',
   songRequestNotificationEmail: 'noahbboyd@gmail.com'
 };
